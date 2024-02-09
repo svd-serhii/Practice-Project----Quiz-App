90bad2c364b15dc83e44616773f69efb5739b989
Serhii Demchenko <svdemchenko@gmail.com>
2024-02-09 22:23:36 +0200

    add quiz state

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,30 @@
 import React, { useState } from "react";
+import QUESTIONS from "../questions";
 
 const Quiz = () => {
-	const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+	// const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
 	const [userAnswers, setUserAnswers] = useState([]);
 
-	return <p>Currently active Quiz</p>;
+	const activeQuestionIndex = userAnswers.length;
+
+	function handleSelectAnswer(selectedAnswer) {
+		setUserAnswers((prevUserAnswer) => {
+			return [...prevUserAnswer, selectedAnswer];
+		});
+	}
+
+	return (
+		<div id="question">
+			<h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+			<ul id="answers">
+				{QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+					<li key={answer} className="answer">
+						<button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 };
 
 export default Quiz;
