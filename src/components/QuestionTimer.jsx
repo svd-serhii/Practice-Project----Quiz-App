1a7e57b30f68f10134f90a65d792237db6c33669
Serhii Demchenko <svdemchenko@gmail.com>
2024-02-13 22:25:37 +0200

    Update QuestionTimer.jsx

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -4,7 +4,7 @@ const QuestionTimer = ({ timeout, onTimeout, mode }) => {
 	const [remainingTime, setRemainingTime] = useState(timeout);
 
 	useEffect(() => {
-		console.log("SETTING TIMEOUT");
+		// console.log("SETTING TIMEOUT");
 		const timer = setTimeout(onTimeout, timeout);
 		return () => {
 			clearTimeout(timer);
@@ -12,7 +12,7 @@ const QuestionTimer = ({ timeout, onTimeout, mode }) => {
 	}, [timeout, onTimeout]);
 
 	useEffect(() => {
-		console.log("SETTING INTERVAL");
+		// console.log("SETTING INTERVAL");
 		const interval = setInterval(() => {
 			setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
 		}, 100);
